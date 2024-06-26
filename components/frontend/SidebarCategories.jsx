5ab31c6f0c3d02dba8db186a013224156c15dfbb
BDyana <info.bdyana@gmail.com>
2024-06-27 00:28:17 +0600

    change margin

--- components/frontend/SidebarCategories.jsx
+++ components/frontend/SidebarCategories.jsx
@@ -15,7 +15,7 @@ export default async function SidebarCategories() {
       <h2 className="bg-slate-100 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100">
         Shop By Category ({categories.length})
       </h2>
-      <div className="py-3 px-6 h-[300px] overflow-y-auto flex flex-col gap-2">
+      <div className="py-3 px-3 h-[300px] overflow-y-auto flex flex-col gap-2">
         {categories.length > 0 &&
           categories.map((category, i) => {
             return (
@@ -25,8 +25,8 @@ export default async function SidebarCategories() {
                 className="flex items-center gap-3 hover:bg-slate-50 duration-300 transition-all dark:text-slate-300 dark:hover:bg-slate-600 rounded-md"
               >
                 <Image
-                  width={556}
-                  height={556}
+                  width={500}
+                  height={500}
                   className="w-10 h-10 rounded-full object-cover border border-lime-300"
                   src={category.imageUrl}
                   alt={category.title}
