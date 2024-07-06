61fcf3b76fc4747d5d97ee0f103b2dd7ff066cae
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-06 14:19:58 +0600

    Update CategoryGrid.jsx

--- components/frontend/CategoryGrid.jsx
+++ components/frontend/CategoryGrid.jsx
@@ -8,13 +8,23 @@ export default async function CategoryGrid() {
   const categories = categoriesData.filter(
     (category) => category.products.length > 0
   );
+
+  const selectRandomCategories = (categories, count) => {
+    let shuffled = categories.sort(() => 0.5 - Math.random());
+
+    return shuffled.slice(0, count);
+  };
+
+  const newCategories = selectRandomCategories(categories, 16);
   // console.log(categories);
   return (
     <div className="sm:col-span-3 sm:block bg-white border border-gray-300 rounded-sm  dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden hidden">
-      <h2 className="bg-slate-100 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100">Shop By Category</h2>
-      <div className="flex grid grid-cols-8 text-center items-center px-5 py-8 gap-x-3 gap-y-8">
-        {categories.length > 0 &&
-          categories.map((category, i) => {
+      <h2 className="bg-slate-100 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100">
+        Shop By Category
+      </h2>
+      <div className="grid grid-cols-8 text-center items-center px-5 py-8 gap-x-3 gap-y-8">
+        {newCategories.length > 0 &&
+          newCategories.map((category, i) => {
             return (
               <Link
                 key={i}
