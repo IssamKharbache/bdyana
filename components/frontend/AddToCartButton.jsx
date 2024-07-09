932827cd908387f173d62b6cc3868e3dbef2e6a5
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-09 16:18:55 +0600

    Update AddToCartButton.jsx

--- components/frontend/AddToCartButton.jsx
+++ components/frontend/AddToCartButton.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { addToCart } from "@/redux/slices/cartSlice";
-import { BaggageClaim } from "lucide-react";
+import { ShoppingCart  } from "lucide-react";
 import React from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
@@ -17,7 +17,7 @@ export default function AddToCartButton({ product }) {
       onClick={() => handleAddToCart()}
       className="flex items-center space-x-2 bg-lime-600 px-4 py-2 rounded-md text-white"
     >
-      <BaggageClaim />
+      <ShoppingCart size={18} />
       <span>Add to Cart</span>
     </button>
   );
