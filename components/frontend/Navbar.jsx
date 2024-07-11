ada59214dff643fb8239ff74d95aa9184718667e
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-12 01:25:20 +0600

    Update Navbar.jsx

--- components/frontend/Navbar.jsx
+++ components/frontend/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   return (
     <div className="bg-white dark:bg-slate-700">
-      <div className="container flex items-center justify-between pt-5 px-0 max-w-6xl gap-8 mx-auto">
+      <div className="container flex items-center justify-between pt-5 lg:px-0 px-5 max-w-6xl gap-8 mx-auto">
         {/* Logo */}
         <Link className="" href="/">
           <Image src={logo} alt="BDyana logo" className="w-40" />
@@ -43,12 +43,12 @@ export default function Navbar() {
             )}
             <HelpModal />
             <CartCount />
-            <ThemeSwitcherBtn />
+        <ThemeSwitcherBtn />
           </div>
         </div>
-        <div className="container mx-auto mt-4">
-          <div className="flex w-full md:hidden">
-              <div className="flex w-full overflow-hidden rounded-md">
+        <div className="container w-full mx-auto mt-4">
+          <div className="flex w-full md:hidden mx-auto">
+              <div className="flex overflow-hidden rounded-md">
                 <SearchForm />
               </div>
           </div>
