a6d9b6ae792713b34ff273b25baa989845b7a63a
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-25 10:28:22 +0600

    Update Product.jsx

--- components/frontend/Product.jsx
+++ components/frontend/Product.jsx
@@ -3,6 +3,7 @@ import { addToCart } from "@/redux/slices/cartSlice";
 import { ShoppingCart  } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 
