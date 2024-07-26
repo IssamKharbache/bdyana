4decfe04e1a8e822bbed5beb3b9694b0767c6fdb
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-26 21:18:13 +0600

    Update FilteredProducts.jsx

--- components/frontend/Filter/FilteredProducts.jsx
+++ components/frontend/Filter/FilteredProducts.jsx
@@ -12,7 +12,7 @@ export default async function FilteredProducts({
   const totalPages = Math.ceil(productCount / pageSize);
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1">
         {products.map((product, i) => {
           return <Product product={product} key={i} />;
         })}
