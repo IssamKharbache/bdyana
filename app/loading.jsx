7f9e84d13c65a6ac6ef3a9c6e8ff84b63737d741
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-26 17:19:15 +0600

    Update loading.jsx

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../public/Logo.png";
 
 export default function Loading() {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className="w-screen h-screen flex items-center">
+      <div className="text-center m-auto">
+        <Image src={logo} alt="BDyana logo" className=""/>
+      </div>
     </div>
   );
 }
