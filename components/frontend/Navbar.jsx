b3388cc7e207e167dfaf109b27dab2f2e4b2f022
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-26 21:44:09 +0600

    Update Navbar.jsx

--- components/frontend/Navbar.jsx
+++ components/frontend/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
             <SearchForm />
             </div>
           </div>
-          <div className="flex gap-4 lg:gap-2">
+          <div className="flex gap-2 lg:gap-4">
             {status === "unauthenticated" ? (
               <Link
                 href="/login"
