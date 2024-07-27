45d423c7ed21c1fa4c3000e4a47678dd774abaed
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-27 13:34:03 +0600

    Update CategoryList.jsx

--- components/frontend/CategoryList.jsx
+++ components/frontend/CategoryList.jsx
@@ -5,8 +5,8 @@ import { MoveRight } from 'lucide-react';
 
 export default function CategoryList({ category, isMarketPage }) {
   return (
-    <div className="bg-white border border-gray-300 rounded-lg  dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
-      <div className="bg-slate-100 dark:bg-gray-800 py-1 px-4 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100 flex justify-between items-center">
+    <div className="bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
+      <div className="bg-slate-100 dark:bg-gray-800 py-1 pl-4 pr-0 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100 flex justify-between items-center">
         <h2>{category.title}</h2>
         <Link
           className="duration-300 transition-all text-slate-800 dark:text-slate-100 rounded-md px-4 py-2"
@@ -15,7 +15,7 @@ export default function CategoryList({ category, isMarketPage }) {
           <MoveRight />
         </Link>
       </div>
-      <div className="bg-white dark:bg-slate-700 py-4 px-2">
+      <div className="bg-white dark:bg-slate-700 pt-2 px-1 pb-0">
         <CategoryCarousel
           isMarketPage={isMarketPage}
           products={category.products}
