b1ffdfe2905a56d19c66c8b679839ee3a1980b9b
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-27 19:31:27 +0600

    Update Breadcrumb.jsx

--- components/frontend/Breadcrumb.jsx
+++ components/frontend/Breadcrumb.jsx
@@ -10,7 +10,7 @@ export default function Breadcrumb() {
   pathArr.shift();
   console.log(pathArr);
   return (
-    <nav className="flex mb-8" aria-label="Breadcrumb">
+    <nav className="flex mb-5" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
         <li className="inline-flex items-center">
           <Link
@@ -26,7 +26,7 @@ export default function Breadcrumb() {
             <li key={i}>
               <div className="flex items-center capitalize">
                 <ChevronRight className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" />
-                <span className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white ">
+                <span className="ms-1 text-sm font-light text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
                   {item}
                 </span>
               </div>
