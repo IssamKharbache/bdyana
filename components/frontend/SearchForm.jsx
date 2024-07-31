5cc5c4232c4e955ea2a4d2a33e75071303aa44be
BDyana <168805526+BDyana@users.noreply.github.com>
2024-07-31 07:57:23 +0600

    Update SearchForm.jsx

--- components/frontend/SearchForm.jsx
+++ components/frontend/SearchForm.jsx
@@ -27,7 +27,7 @@ export default function SearchForm() {
           {...register("searchTerm")}
           type="text"
           id="voice-search"
-          className="class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-gray-500 focus:border-gray-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-gray-500 focus:border-gray-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
           placeholder="Search Products, Categories, Markets..."
           required
         />
