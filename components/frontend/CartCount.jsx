5fe092922fa2ff42292837a638b7f762c094fa89
BDyana <168805526+BDyana@users.noreply.github.com>
2024-08-06 14:22:08 +0600

    Update CartCount.jsx

--- components/frontend/CartCount.jsx
+++ components/frontend/CartCount.jsx
@@ -11,7 +11,7 @@ export default function CartCount() {
       href="/cart"
       className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg "
     >
-      <ShoppingCart className="text-lime-700 dark:text-lime-500" />
+      <ShoppingCart className="text-slate-700 dark:text-lime-500" />
       <span className="sr-only">Cart</span>
       <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-1 right-0 dark:border-gray-900">
         {cartItems.length}
