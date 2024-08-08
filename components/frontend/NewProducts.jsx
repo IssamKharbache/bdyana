cae7febfed34d066b71594916829c58d2d2e9ee8
BDyana <168805526+BDyana@users.noreply.github.com>
2024-08-08 12:15:00 +0600

    Update NewProducts.jsx

--- components/frontend/NewProducts.jsx
+++ components/frontend/NewProducts.jsx
@@ -18,7 +18,7 @@ export default function NewProduct() {
     <div className="py-12">
       <h2 class="text-xl">New Arrivals</h2>
       <h4>100+ products added today</h4>
-      <div className="flex grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 mt-4">
+      <div className="flex grid grid-cols-3 lg:grid-cols-6 md:grid-cols-4 mt-4">
       {productsList.map((product) => (
         <div key={product.id}>
           <Product product={product}/>
